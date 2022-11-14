488ef5b070b7d6b85be6a6950655445fba3ec10d
danhfa1999 <danh260199@gmail.com>
2022-11-14 16:12:17 +0700

    chore:add font and build body

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,5 @@
 //main.js file
+import barba from "@barba/core";
+import barbaCss from "@barba/css";
+
+console.log(barba, barbaCss);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,35 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit', // Just-In-Time Compiler
-  purge: ['./src/**/*.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.html"],
   theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "max-xl": { max: "1279px" },
+        "max-lg": { max: "1023px" },
+        "max-md": { max: "767px" },
+        "max-sm": { max: "639px" },
+      },
+      colors: {
+        primary: "#0F4C81",
+        secondary: "#F9F9F9",
+      },
+      fontFamily: {
+        Grotesk: ["Space Grotesk", "sans-serif"],
+        IBM: ["IBM Plex Sans", "sans-serif"],
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
