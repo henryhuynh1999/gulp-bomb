4cfd3dd4558a63a52519fd8d27e41015600327b0
danhfa1999 <danh260199@gmail.com>
2022-11-18 12:30:33 +0700

    chore:change path

--- src/js/main.js
+++ src/js/main.js
@@ -3,28 +3,6 @@ $(document).ready(function () {
     glare: true,
     maxGlare: 0.7,
   });
-  const control_dropdown = document.getElementById("control-dropdown");
-  const dropwdown = document.querySelector(".dropdown");
-  const backtotop = document.querySelector(".is-hidden");
-  window.onscroll = function () {
-    console.log("In onscroll");
-    backtop();
-  };
-  function backtop() {
-    if (window.scrollTo > 400) {
-      backtotop.classList.remove("is-hidden");
-    } else {
-      backtotop.classList.add("is-hidden");
-    }
-  }
-  // control_dropdown.addEventListener("click", () => {
-  //   dropwdown.classList.toggle("active");
-  // });
-  function initScript() {
-    // Add your scripts here
-    // Example: initSlider();
-  }
-  initScript();
 });
 $(window).scroll(function () {
   if ($(this).scrollTop() > 50) {
@@ -32,11 +10,6 @@ $(window).scroll(function () {
   } else {
     $(".back-to-top").fadeOut();
   }
-  // if ($(this).scrollTop() > 150) {
-  //   $(".page-header").addClass("sticky top-0");
-  // } else {
-  //   $(".page-header").removeClass("sticky top-0");
-  // }
 });
 $(".back-to-top").on("click", function (e) {
   e.preventDefault();
